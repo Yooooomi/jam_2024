ed16e0d4db79b99c72a34ca89d31a175b74c2e8e
noerls <noe.rivals@gmail.com>
2024-01-26 23:51:01 +0100

    Add juggling and the crazy, amazing, perfecly abstracted Dot system

---
+++ Assets/Scripts/Juggle.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Juggle : MonoBehaviour
+{
+    public SpeedModifier playerSpeedModifier;
+    public PlayerControls playerControls;
+    public float juggleDuration = 1.0f;
+
+    private float jugglingLeftTime = 0.0f;
+
+    void StartToJuggle()
+    {
+        jugglingLeftTime = juggleDuration;
+        playerSpeedModifier.ApplyDot(new SpeedDot(juggleDuration, 0.0f));
+    }
+
+    void StopJuggle()
+    {
+        jugglingLeftTime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jugglingLeftTime > 0.0f)
+        {
+            jugglingLeftTime -= Time.deltaTime;
+            if (jugglingLeftTime <= 0.0f)
+            {
+                StopJuggle();
+            }
+        }
+        else if (playerControls.secondaryAction)
+        {
+            StartToJuggle();
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,17 @@ public class PlayerMovement : MonoBehaviour
 {
     private PlayerControls controls;
     public Rigidbody2D rigidbody2d;
-    public float moveSpeed;
+    public SpeedModifier speedModifier;
+    public float moveSpeed = 5.0f;
 
-    private void Start() {
+    private void Start()
+    {
         controls = GetComponent<PlayerControls>();
     }
 
-    private void FixedUpdate() {
-        Vector2 movement = new Vector2(controls.direction.x, controls.direction.y) * moveSpeed * Time.deltaTime;
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(controls.direction.x, controls.direction.y) * moveSpeed * speedModifier.getValue() * Time.deltaTime;
         rigidbody2d.MovePosition(movement + new Vector2(transform.position.x, transform.position.y));
     }
 }
