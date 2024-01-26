d88dd262c67030c9b75036e2323d4ceb9bfbcf36
noerls <noe.rivals@gmail.com>
2024-01-26 22:03:21 +0100

    Add collision

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerControls controls;
+    public Rigidbody2D rigidbody2d;
     public float moveSpeed;
 
     private void Start() {
         controls = GetComponent<PlayerControls>();
     }
 
-    private void Update() {
-        transform.position += new Vector3(controls.direction.x, controls.direction.y, 0) * moveSpeed * Time.deltaTime;
+    private void FixedUpdate() {
+        Vector2 movement = new Vector2(controls.direction.x, controls.direction.y) * moveSpeed * Time.deltaTime;
+        rigidbody2d.MovePosition(movement + new Vector2(transform.position.x, transform.position.y));
     }
 }
